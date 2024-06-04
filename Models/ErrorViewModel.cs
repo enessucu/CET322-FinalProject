93ca91cdc7c583837aeda3fa1dce4d2e11fb5611
enessucu <enesimo41@gmail.com>
2024-06-04 23:44:30 +0300

    add completed project files to git

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace iTechnoStore322Final.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using iTechnoStore322Final.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace iTechnoStore322Final.Services
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
